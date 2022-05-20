33d36a2b19b65af7c66ff1b72ec6084083ac3b57
Руслан Чежин <4ezhin@gmail.com>
2022-05-20 13:24:01 +0300

    project

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -12,3 +12,12 @@ def page_not_found(request, exception):
 
 def csrf_failure(request, reason=''):
     return render(request, 'core/403csrf.html')
+
+
+def internal_server_error(request, exception):
+    return render(
+        request,
+        'core/500.html',
+        {'path': request.path},
+        status=500
+    )
